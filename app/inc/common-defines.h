9301cfde672a2159f0fc582270c9b583d418b7c1
RajDaBest <imagination12344321.com@gmail.com>
2024-10-29 20:29:24 +0530

    added libopencm3 in the source; added it to the vscode includes and the makefile compiler includes

--- app/inc/common-defines.h
+++ app/inc/common-defines.h
@@ -3,5 +3,6 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 #endif // INC_COMMON_DEFINES_H
---
+++ app/src/firmware.c
@@ -0,0 +1,13 @@
+#include "../inc/common-defines.h"
+#include <libopencm3/stm32/rcc.h> // i have added the libopencm3 folder in the vscode include paths
+// i have also added that folder to the makefile include file that calls the compiler so as to recognize these includes
+
+int main(void)
+{
+    while (true)
+    {
+    }
+
+    // never return
+    return EXIT_SUCCESS;
+}
